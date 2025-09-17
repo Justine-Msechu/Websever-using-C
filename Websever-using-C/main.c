36983e1c107ad2a4265ed0f3c53eea293771daf8
Your Name <justinemsechu12@yahoo.com>
2025-09-17 05:36:03 +0300

    Initial Commit

---
+++ Websever-using-C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Websever-using-C
+//
+//  Created by Justine msechu on 17/09/2025.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
